708c7d8cec7ae0f9a7b99c7f5d1479b3618e20e6
takashimamorino <does1026@gmail.com>
2021-11-05 14:21:08 +0900

    feat: init parent app

---
+++ parent-app/parent-app/parent_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  parent_appApp.swift
+//  parent-app
+//
+//  Created by takashima on 2021/11/05.
+//
+
+import SwiftUI
+
+@main
+struct parent_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
